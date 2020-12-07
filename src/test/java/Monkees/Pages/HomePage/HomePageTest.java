3bc95f30cd6b32ac3ef77c3035af905f84c24af4
istvangercsak <istvan.gercsak@gmail.com>
2020-12-07 13:33:56 +0000

    Finish Jenkins integration and project description

--- src/test/java/Monkees/Pages/HomePage/HomePageTest.java
+++ src/test/java/Monkees/Pages/HomePage/HomePageTest.java
@@ -25,19 +25,23 @@ public class HomePageTest extends HomePagePOM {
 
     @Test
     public void checkNavigationBar() {
+        logger.info("Start: Navigation bar test on the Home Page");
         generalTests.checkHrefForHomeButton(driver);
         generalTests.checkHrefForTourInfoButton(driver);
         generalTests.checkHrefForBandButton(driver);
         generalTests.checkHrefForMediaButton(driver);
         generalTests.checkHrefForContactButton(driver);
+        logger.info("End: Navigation bar test on the Home Page");
     }
 
     @Test
     public void checkFooterContent() {
+        logger.info("Start: Footer bar test on the Home Page");
         generalTests.checkHrefForFacebookIcon(driver);
         generalTests.checkHrefForTwitterIcon(driver);
         generalTests.checkHrefForYouTubeIcon(driver);
         generalTests.checkHrefForSpotifyIcon(driver);
+        logger.info("End: Footer bar test on the Home Page");
     }
 
     @Test
@@ -46,9 +50,11 @@ public class HomePageTest extends HomePagePOM {
     }
 
     public void checkBackgroundImageIsVisibleAndAttributes() {
+        logger.info("Start: Background image test on the Home Page");
         backgroundPictureElement(driver).isEnabled();
         backgroundPictureElement(driver).isDisplayed();
         checkAttributes(backgroundPictureElement(driver), backgroundPictureSRC, backgroundPictureAlt);
+        logger.info("End: Background image test on the Home Page");
     }
 
     public void checkAttributes(WebElement element, String src, String alt) {
@@ -59,6 +65,7 @@ public class HomePageTest extends HomePagePOM {
     @AfterMethod
     public void tearDown() {
         driver.quit();
+        logger.info("Browser closed. Test finished.");
     }
 
 }
